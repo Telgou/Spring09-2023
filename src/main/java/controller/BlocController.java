7021f8377a2dd9b72350ec5428a0a6dad614cc9e
Telgou <gamgamiahmed@yahoo.fr>
2023-10-29 17:53:50 +0100

    d

--- src/main/java/controller/BlocController.java
+++ src/main/java/controller/BlocController.java
@@ -1,12 +1,21 @@
 package controller;
 
+import com.example.spring092023.entities.Bloc;
+import com.example.spring092023.entities.Universite;
+import com.example.spring092023.services.BlocService;
+import com.example.spring092023.services.UniversiteService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
-import com.example.spring092023.repositories.BlocRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @RestController
 @AllArgsConstructor
 @RequestMapping("/blocs")
-@RequestMapping("/blocs")
 public class BlocController {
-    private BlocRepository blocService;
+    private BlocService blocService;
+<<<<<<< HEAD
+
+=======
+>>>>>>> fdfcbf6a48b2071813d968accdee75a0ece7e492
 }
--- src/main/java/controller/ChambreController.java
+++ src/main/java/controller/ChambreController.java
@@ -3,12 +3,22 @@ package controller;
 import com.example.spring092023.entities.Bloc;
 import com.example.spring092023.services.ChambreService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
-import com.example.spring092023.services.iChambreService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @AllArgsConstructor
 @RequestMapping("/chambres")
 public class ChambreController {
+    private ChambreService chambreService;
 
-    private iChambreService chambreService;
+    @PostMapping("/setchambres/{nombloc}")
+    public Bloc setchambres(@PathVariable("nombloc") String nombloc, @RequestBody Map<String, List<Long>> requestBody) {
+        List<Long> numchambre = requestBody.get("numchambre");
+        return chambreService.affecterChambresABloc(numchambre,nombloc);
+    }
+
+    private ChambreService chambreService;
 }
